7b9d537b59153c011d2f208a14691e88fb84aeb3
cwistedt <carl.wistedt@gmail.com>
2016-10-23 17:13:12 +0200

    Commiting Flyweight example Money

---
+++ com/cwlib/DesignPatterns/Flyweight/FlyweightUserCache/FlyweightUser.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns.Flyweight.FlyweightUserCache
+{
+    class FlyweightUser
+    {
+        public Guid UserID { get; set; }
+        public string Username { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public List<FlyweightUser> Followers { get; set; }
+
+        public static FlyweightUser GetUser(Guid userID)
+        {
+            // get user from the database
+
+            // demo user with random id
+            return new FlyweightUser
+            {
+                UserID = Guid.NewGuid()
+            };
+        }
+
+    }
+}
---
+++ com/cwlib/DesignPatterns/Flyweight/FlyweightUserCache/FlyweightUserFactory.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns.Flyweight.FlyweightUserCache
+{
+    class FlyweightUserFactory
+    {
+        public static int UsersCount = 0;
+        private Dictionary<Guid, FlyweightUser> FlyweightUsers = new Dictionary<Guid, FlyweightUser>();
+
+        public FlyweightUser GetUser(Guid userID)
+        {
+            if (FlyweightUsers.ContainsKey(userID))
+                return FlyweightUsers[userID];
+
+            FlyweightUser user = FlyweightUser.GetUser(userID);
+            FlyweightUsers.Add(userID, user);
+            UsersCount++;
+            return user;
+                
+        } 
+    }
+}
